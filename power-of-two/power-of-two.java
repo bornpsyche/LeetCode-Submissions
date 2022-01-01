59fe4697919923f8ea08237ad69851ce84f55751
Ashish Pradhan <87824832+bornpsyche@users.noreply.github.com>
2022-01-02 00:52:40 +0530

    Time: 1 ms (99.98%), Space: 35.8 MB (88.79%) - LeetHub

--- power-of-two/power-of-two.java
+++ power-of-two/power-of-two.java
@@ -4,6 +4,7 @@ class Solution {
         if(n<=0) return false;
         while(n!=0){
             if((n&1)==1) flag++;
+            if(flag == 2) break;
             n=n>>1;
         }
         if(flag==1) return true;

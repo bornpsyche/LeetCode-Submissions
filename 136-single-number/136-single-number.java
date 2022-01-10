ff3b0a5cfe2cc928789ae4b82c6f33cbdbf1345b
Ashish Pradhan <87824832+bornpsyche@users.noreply.github.com>
2022-01-10 19:38:09 +0530

    Time: 2 ms (53.96%), Space: 47.6 MB (19.37%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,18 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++) {
-            int temp = nums[i];
-            int cnt = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[j] == temp) cnt++;
-            }
-            list.add(cnt);
-        }
-        int flag = 0;
-        for(int i = 0; i < list.size(); i++) {
-            if(list.get(i) == 1) flag=i; 
-        }
-        return nums[flag];
+        // List<Integer> list = new ArrayList<>();
+        // for(int i = 0; i < nums.length; i++) {
+        //     int temp = nums[i];
+        //     int cnt = 0;
+        //     for(int j = 0; j < nums.length; j++) {
+        //         if(nums[j] == temp) cnt++;
+        //     }
+        //     list.add(cnt);
+        // }
+        // int flag = 0;
+        // for(int i = 0; i < list.size(); i++) {
+        //     if(list.get(i) == 1) flag=i; 
+        // }
+        // return nums[flag];
+        int nonRepeating = nums[0];
+        for(int i = 1; i < nums.length; i++) nonRepeating ^= nums[i];
+        return nonRepeating;
     }
 }

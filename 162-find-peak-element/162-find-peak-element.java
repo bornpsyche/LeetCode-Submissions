4fe2f5c92b709eaf01acf10846fcedf0f6bc6c6e
Ashish Pradhan <87824832+bornpsyche@users.noreply.github.com>
2022-01-13 16:07:12 +0530

    Time: 0 ms (100.00%), Space: 39 MB (38.73%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -6,12 +6,12 @@ class Solution {
 		while(start < end) {
 			int mid = start + (end - start)/2;
 
-			if(nums[mid] < nums[mid+1])
-				start = mid + 1;
-			else
+			if(nums[mid] > nums[mid+1])
 				end = mid;
-			
+			else if (nums[mid] < nums[mid+1]) 
+				start = mid + 1;
 		}
+		
 		return start;
     }
 }

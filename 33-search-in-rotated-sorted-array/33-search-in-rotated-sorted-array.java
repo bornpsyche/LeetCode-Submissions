dfd0374f738dd951c8f1c9a07fd9766c2983bb22
Ashish Pradhan <87824832+bornpsyche@users.noreply.github.com>
2022-01-14 16:29:00 +0530

    Time: 0 ms (100.00%), Space: 38.5 MB (55.76%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -30,23 +30,7 @@ class Solution {
     }
     
     static int findPivot(int[] nums) {
-// 		int start = 0;
-// 		int end = nums.length - 1;
-
-// 		while(start < end) {
-// 			int mid = start + (end - start)/2;
-
-// 			if(nums[mid] > nums[mid+1])
-// 				return mid;
-// 			else if (mid>start && nums[mid] < nums[mid-1]) 
-// 				return mid-1;
-// 			else if(start >= mid)
-// 				end = mid-1;
-// 			else if(start<mid)
-// 				start = mid+1;
-// 		}
-// 		return -1;
-        int start = 0;
+		int start = 0;
         int end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
@@ -58,28 +42,10 @@ class Solution {
                 return mid-1;
             }
 
-            // if elements at middle, start, end are equal then just skip the duplicates
-            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
-                // skip the duplicates
-                // NOTE: what if these elements at start and end were the pivot??
-                // check if start is pivot
-                if (start < end && nums[start] > nums[start + 1]) {
-                    return start;
-                }
-                start++;
-
-                // check whether end is pivot
-                if (end > start && nums[end] < nums[end - 1]) {
-                    return end - 1;
-                }
-                end--;
-            }
-            // left side is sorted, so pivot should be in right
-            else if(nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
-                start = mid + 1;
-            } else {
+            if(nums[start] >= nums[mid]) 
                 end = mid - 1;
-            }
+            if(nums[start] < nums[mid]) 
+                start = mid + 1;
         }
         return -1;
 	}

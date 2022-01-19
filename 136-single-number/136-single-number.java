1f3aa3659df38ff0baada7af2312ecb04d0cd655
Ashish Pradhan <87824832+bornpsyche@users.noreply.github.com>
2022-01-19 16:52:00 +0530

    Time: 1 ms (95.24%), Space: 47.5 MB (37.28%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int nonRepeating = 0;
-        for(int i = 0; i < nums.length; i++) nonRepeating ^= nums[i];
+        for(int n : nums) nonRepeating ^= n;
         return nonRepeating;
     }
 }
